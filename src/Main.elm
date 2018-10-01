1298e139103ec9665c06422ca5343f6601041f43
Kevin Ndung'u <kevgathuku@gmail.com>
2018-10-01 19:45:17 +0300

    Browser.sandbox -> Browser.document

--- src/Main.elm
+++ src/Main.elm
@@ -14,34 +14,44 @@ initialModel =
     { count = 0 }
 
 
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
 type Msg
     = Increment
     | Decrement
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            { model | count = model.count + 1 }
+            ( { model | count = model.count + 1 }, Cmd.none )
 
         Decrement ->
-            { model | count = model.count - 1 }
+            ( { model | count = model.count - 1 }, Cmd.none )
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
+    { title = "Elm app"
+    , body =
+        [ div []
+            [ button [ onClick Increment ] [ text "+1" ]
+            , div [] [ text <| String.fromInt model.count ]
+            , button [ onClick Decrement ] [ text "-1" ]
+            ]
         ]
+    }
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.document
+        { init = \_ -> init
         , view = view
         , update = update
+        , subscriptions = always Sub.none
         }
